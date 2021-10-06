f85a2c75bc1b58d0761bcf418e3eabe79b7fe2b4
jiyeong1004 <s2019s28@e-mirim.hs.kr>
2021-10-07 03:05:41 +0900

    #4 4장 완료 커밋
    useHistory로 로그아웃 후 주소 이동하기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,13 @@
 import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
 
 const Profile = () => {
-    const onLogOutClick = () => authService.signOut();
+    const history = useHistory();
+
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    };
 
     return (
         <>
