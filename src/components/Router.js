554ae7d077734db7f0bf51701b7042c88c5471b4
jiyeong1004 <s2019s28@e-mirim.hs.kr>
2021-10-06 17:35:45 +0900

    #4 내비게이션 추가하고 로그아웃 처리하기
    내비게이션 컴포넌트 만들고 라우터에 추가하기

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,12 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
                     <Route exact path = "/">
