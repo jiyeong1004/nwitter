bf41e559df9b181faffda25f4a62ba98d9183ecb
jiyeong1004 <s2019s28@e-mirim.hs.kr>
2021-10-06 17:46:36 +0900

    #4 내비게이션에 링크 추가하기

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
@@ -9,9 +10,14 @@ const AppRouter = ({ isLoggedIn }) => {
             {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
+                <>
                     <Route exact path = "/">
                         <Home />
                     </Route>
+                    <Route exact path = "/profile">
+                        <Profile />
+                    </Route>
+                </>
                 ) : (
                     <Route exact path = "/">
                         <Auth />
